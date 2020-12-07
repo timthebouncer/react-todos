4cc52a184275063910b6aafa804138f3df51d3ca
tim.wty <tim.wty@link8tech.tw>
2020-12-07 18:00:16 +0800

    react-todos-1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
+import InputText from "./Components/Input";
+import Table from "./Components/Table";
+
+const { useState, useEffect } = React;
+const header = [{ id: 1, name: "frank", createDate: new Date(), editable:false,status: "" }];
 
 function App() {
+  const [state, setState] = useState([]);
+  // const [disable, setDisable] = useState(true);
+  useEffect(
+    function () {
+      setTimeout(function () {
+        setState(header);
+      }, 300);
+      //能夠引起useEffect的依賴
+    }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>ToDo List</h1>
+      <InputText state={state} setState={setState}/>
+      <Table state={state} setState={setState}/>
     </div>
   );
 }
---
+++ src/Components/FontawsomeIcons/index.js
@@ -0,0 +1,6 @@
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faCheckSquare, faCoffee } from '@fortawesome/free-solid-svg-icons'
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
+library.add(faCheckSquare, faCoffee)
+
